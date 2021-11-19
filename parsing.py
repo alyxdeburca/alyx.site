a5bcb8612ec0111fa86e803cd0f1ca28dcb75ad5
Alyx Burke <alyx@alyx.site>
2021-11-19 12:44:12 +0000

    directory shittin

--- parsing.py
+++ parsing.py
@@ -8,7 +8,7 @@ inv_dict = []
 for item in inv["rgDescriptions"]:
     inv_dict.append(inv["rgDescriptions"][item]["name"])
 
-f = open("cs.html", "w")
+f = open("cs/index.html", "w")
 table = """
                         </p>
                     </div>
@@ -23,25 +23,13 @@ html = """
 <link rel="stylesheet"  type="text/css" href="styles/main.css">
 <!-- Google Fonts Stylesheet -->
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Tangerine">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<h3 class="text-center text-info">
-				<a href="https://steamcommunity.com/id/csalyx/inventory/730">Alyx's CS:GO Inventory</a>
-			</h3>
-			<div class="row">
-				<div class="col-md-2">
-					<div class="card-body">
-						<p class="card-text">
+<body>
+	<h3 id="inv">Alyx's CS:GO Inventory</h3>
+	<p id="inv">	
 """ + table + """
-                </p>
-			</div>
-		</div>
-	</div>
-</div>
-
+    </p>
+</body>
 </html>"""
 f.write(html)
